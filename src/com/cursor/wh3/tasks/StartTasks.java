1933031e6f32161456d5d6aef9eec851514653cd
Falcon89 <vasyl.kachala@gmail.com>
2019-06-30 00:18:46 +0300

    add new change in dev branch

--- src/com/cursor/wh3/tasks/StartTasks.java
+++ src/com/cursor/wh3/tasks/StartTasks.java
@@ -2,6 +2,9 @@ package com.cursor.wh3.tasks;
 
 public class StartTasks {
     public StartTasks() {
+        /**
+         * Create method StartTasks, creating class objects
+         */
         ArrayCounts.countsArrayDESC();
         ReplaceArray.replaceValueArrayList();
         PositiveNumbers.sortingAndSumOfAllPositiveNumbers();
